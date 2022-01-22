88ac54e88ffeb1106e2bd4058886e9b3cec6c2d7
victor.almeida.fonseca <victor.fonseca@toroinvestimentos.com.br>
2022-01-21 21:10:16 -0300

    feat: pages para navegação

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export * from './player'
+export * from './squares'
+export * from './start'
+export * from './avatars'
--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,5 +1,12 @@
 import { Square } from "../../components/squares"
+import { generateArray } from '../../utils'
 
 export default function Board() {
-    return <Square selected />  
+  const rows = generateArray(25)  
+
+  return rows.map((row) => {
+    return <>
+      <Square key={row} selected = {!row} />
+      </>
+  }) 
 }
